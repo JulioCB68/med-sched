edf91dd203df907d9b78baf9f478f86b08f866ee
Júlio César <juliobento237@gmail.com>
2024-08-06 00:21:52 -0300

    fix: fixing setup Next Auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,15 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-}
+})
 
-export default NextAuth(authOptions)
+export { handler as GET, handler as POST }
+// eslint-disable-next-line prettier/prettier
+
