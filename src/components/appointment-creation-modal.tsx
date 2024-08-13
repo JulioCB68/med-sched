320374222b1f779543a6ed9fec28019b514b5d6a
Júlio César <juliobento237@gmail.com>
2024-08-13 17:52:25 -0300

    feat: finishing UI from appointment creation modal

--- src/components/appointment-creation-modal.tsx
+++ src/components/appointment-creation-modal.tsx
@@ -4,6 +4,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
+import { DatePicker } from './date-picker'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
@@ -20,7 +21,11 @@ export function AppointmentCreationModal() {
 
       <form action="" className="space-y-4 text-muted">
         <div className="space-y-2">
-          <Label>Paciente</Label>
+          <Label>Nome do paciente</Label>
+          <Input />
+        </div>
+        <div className="space-y-2">
+          <Label>Nome do Médico</Label>
           <Input />
         </div>
         <div className="flex items-center justify-between gap-4">
@@ -37,11 +42,10 @@ export function AppointmentCreationModal() {
           <Label>Motivo</Label>
           <Input />
         </div>
-        <div className="space-y-2">
+        <div className="flex flex-col space-y-2 pb-2">
           <Label>Data</Label>
-          <Input />
+          <DatePicker />
         </div>
-
         <Button className="w-full">Agendar</Button>
       </form>
     </DialogContent>
---
+++ src/components/date-picker.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import * as React from 'react'
+
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+
+import { Calendar as CalendarIcon } from 'lucide-react'
+
+export function DatePicker() {
+  const [date, setDate] = React.useState<Date>()
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-full justify-start text-left font-normal hover:bg-transparent hover:text-muted',
+            !date && 'text-muted-foreground',
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? (
+            format(date, 'P', { locale: ptBR })
+          ) : (
+            <span>Pick a date</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
