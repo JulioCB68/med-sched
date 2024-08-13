87386d1276aa5a4b91556a4333a3a07acb23566c
Júlio César <juliobento237@gmail.com>
2024-08-13 19:08:55 -0300

    feat: adding swlite db with prisma and create auth tables using NextAuth with Prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from 'next-auth'
+import { Adapter } from 'next-auth/adapters'
 import GoogleProvider from 'next-auth/providers/google'
 
+import { db } from '@/lib/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+
 const handler = NextAuth({
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
