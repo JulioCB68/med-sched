3924b1a17ad5796e085438f5045c0fc6771959d7
Júlio César <juliobento237@gmail.com>
2024-08-13 21:26:21 -0300

    feat: change login page to make login only with google

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,8 @@
 'use client'
 
-import Link from 'next/link'
-
 import { signIn } from 'next-auth/react'
 
 import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
 import { ChromeIcon } from 'lucide-react'
@@ -18,61 +13,18 @@ export default function Login() {
       <form className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
           <h1 className="text-3xl font-bold">Bem vindo 👋</h1>
-          <p className="text-dark-700">Começe já com seus agendamentos.</p>
-          <p className="mt-2 text-sm">
-            <Link
-              href="/register"
-              className="text-sm text-primary-foreground/80 underline underline-offset-4 hover:text-primary-foreground"
-            >
-              Ainda não tem uma conta?
-            </Link>
-          </p>
+          <p className="text-dark-700">Começe a gerenciar suas consultas.</p>
         </section>
 
-        <div className="space-y-4">
-          <Label htmlFor="full-name" className="text-sm">
-            Nome completo
-          </Label>
-          <Input id="full-name" placeholder="Nome completo" />
-        </div>
-
-        <div className="space-y-4">
-          <Label htmlFor="email" className="text-sm">
-            E-mail
-          </Label>
-          <Input id="email" placeholder="Seu melhor e-mail" />
-        </div>
-
-        <div className="space-y-4">
-          <Label htmlFor="password" className="text-sm">
-            Senha
-          </Label>
-          <Input id="password" placeholder="**********" />
-        </div>
-
-        <div className="flex items-center space-x-2">
-          <Checkbox id="terms" className="bg-accent" />
-          <Label htmlFor="terms" className="text-sm">
-            Eu concordo com os{' '}
-            <Link
-              href="#"
-              className="text-sm text-primary-foreground/80 underline underline-offset-4 hover:text-primary-foreground"
-            >
-              Termos & Condições
-            </Link>
-          </Label>
-        </div>
-        <Button className="w-full" size={'lg'}>
-          Entrar
-        </Button>
         <div className="mt-4 flex items-center justify-center space-x-2">
-          <Separator className="h-px w-14 bg-gray-300" />
-          <span className="text-sm">Ou continue com o Google</span>
-          <Separator className="h-px w-14 bg-gray-300" />
+          <Separator className="h-px w-20 bg-gray-300" />
+          <span className="text-center text-sm">
+            Faça login com o Google para continar
+          </span>
+          <Separator className="h-px w-20 bg-gray-300" />
         </div>
         <div className="mt-4 flex justify-center space-x-4">
           <Button
-            variant="secondary"
             size={'lg'}
             className="flex w-full items-center space-x-2"
             onClick={(e) => {
@@ -80,8 +32,8 @@ export default function Login() {
               signIn('google')
             }}
           >
-            <ChromeIcon className="h-5 w-5" />
-            <span>Google</span>
+            <ChromeIcon className="h-5 w-5 text-black/70" />
+            <span className="text-black/90">Google</span>
           </Button>
         </div>
       </form>
