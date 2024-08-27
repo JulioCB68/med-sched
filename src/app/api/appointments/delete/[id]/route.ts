c6051ee1f88b9f85a8d67ddd9fb7bf262cc76cc8
Júlio César <juliobento237@gmail.com>
2024-08-26 23:37:33 -0300

    feat: deleting appointment in google calendar

--- src/app/api/appointments/delete/[id]/route.ts
+++ src/app/api/appointments/delete/[id]/route.ts
@@ -1,4 +1,6 @@
+import { getGoogleOAuthToken } from '@/lib/google'
 import { db } from '@/lib/prisma'
+import { google } from 'googleapis'
 import { NextResponse } from 'next/server'
 
 export async function DELETE(
@@ -11,11 +13,68 @@ export async function DELETE(
     return NextResponse.json({ error: 'ID is required' }, { status: 400 })
   }
 
-  const deleteAppointment = await db.appointment.delete({
+  const user = await db.user.findFirstOrThrow({
     where: {
-      id,
+      Appointment: {
+        some: {
+          id,
+        },
+      },
     },
   })
 
-  return NextResponse.json(deleteAppointment, { status: 200 })
+  async function listEvents() {
+    const calendar = google.calendar({
+      version: 'v3',
+      auth: await getGoogleOAuthToken(user.id),
+    })
+
+    try {
+      const response = await calendar.events.list({
+        calendarId: 'primary',
+        maxResults: 50,
+        orderBy: 'startTime',
+        singleEvents: true,
+      })
+
+      const events = response.data.items
+      if (events && events.length) {
+        events.forEach((event) => {
+          console.log(`Event ID: ${event.id}, Summary: ${event.summary}`)
+        })
+      } else {
+        console.log('No events found.')
+      }
+    } catch (error) {
+      console.error('Error listing events:', error)
+    }
+  }
+
+  await listEvents()
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  try {
+    await calendar.events.delete({
+      calendarId: 'primary',
+      eventId: '33crlllr5jq9cht60o63c5oev4',
+    })
+
+    const deleteAppointment = await db.appointment.delete({
+      where: {
+        id,
+      },
+    })
+
+    return NextResponse.json(deleteAppointment, { status: 200 })
+  } catch (error) {
+    console.error('Error deleting event:', error)
+    return NextResponse.json(
+      { error: 'Failed to delete event' },
+      { status: 500 },
+    )
+  }
 }
