c4b1d5613e730ddd43ea88807b5ae9f0aa05d07f
Júlio César <juliobento237@gmail.com>
2024-08-27 22:23:21 -0300

    fix

---
+++ src/lib/auth-options.ts
@@ -0,0 +1,41 @@
+import NextAuth from 'next-auth'
+import { Adapter } from 'next-auth/adapters'
+import GoogleProvider from 'next-auth/providers/google'
+
+import { db } from '@/lib/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+
+export const authOptions = NextAuth({
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id
+      }
+      return session
+    },
+
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar?error=permissions'
+      }
+
+      return true
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+})
